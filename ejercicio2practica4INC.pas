b8a1713e62f09afbca56603c6a05a69d434f0d97
MaxPower03 <112443365+MaxPower03@users.noreply.github.com>
2022-09-09 10:37:45 -0300

    Update ejercicio2practica4INC.pas

--- ejercicio2practica4INC.pas
+++ ejercicio2practica4INC.pas
@@ -1,4 +1,4 @@
-program ejercicio2practica4;
+program ejercicio2practica4ahoracompleto;
 const
 	cantg=8;
 type	
@@ -37,11 +37,11 @@ begin
 	else
 		begin
 		act:=L;ant:=L;
-		while(act<>nil)and(act^.dato.codigo<L^.dato.codigo)do begin
+		while(act<>nil)and(act^.dato.codigo<aux^.dato.codigo)do begin
 			ant:=act;
 			act:=act^.sig;
 			end;
-		if(act=nil)then begin
+		if(act=L)then begin
 			aux^.sig:=L;
 			L:=aux;
 			end
@@ -55,21 +55,80 @@ end;
 
 procedure generarlistas(var v:vectorg);
 var
-	i:rango8;
 	d:datos;
 begin
 	randomize;
-	for i:=1 to cantg do begin
-		writeln('insete un codigo de peli');
-		readln(d.codigo);
-		while(d.codigo<>0)do begin
-			d.codigog:=i;
-			d.puntaje:=random(100);
-			agregarordenado(v[i],d);
-			writeln('insete un codigo de peli');
-			readln(d.codigo);
-			end;
-		end;	
+	d.codigo:=random(50)-1;
+	while(d.codigo<>-1)do begin
+		d.codigog:=random(cantg)+1;
+		d.puntaje:=random(100);
+		agregarordenado(v[d.codigog],d);
+		d.codigo:=random(50)-1;
+		end;
+end;
+
+procedure agregaratras(var L,ult:lista;d:datos);
+var
+	nuevo:lista;
+begin
+	new(nuevo);
+	nuevo^.sig:=nil;
+	nuevo^.dato.codigo:=d.codigo;
+	nuevo^.dato.puntaje:=d.puntaje;
+	nuevo^.dato.codigog:=d.codigog;
+	if(L=nil)then begin
+		L:=nuevo;
+		ult:=nuevo;
+		end
+	else
+		begin
+			ult^.sig:=nuevo;
+			ult:=nuevo;
+		end;
+end;	
+		
+procedure minimo(var todos:vectorg;var dmin:datos);
+var
+	i,indicemin:integer;
+begin
+	dmin.codigo:=999;
+	for i:=1 to cantg do 
+		if(todos[i]<>nil)then 
+			if(todos[i]^.dato.codigo<=dmin.codigo)then begin
+				indicemin:=i;
+				dmin.codigo:=todos[i]^.dato.codigo;
+				end;
+	if(dmin.codigo<>999)then
+		begin
+			dmin.puntaje:=todos[indicemin]^.dato.puntaje;
+			dmin.codigog:=todos[indicemin]^.dato.codigog;
+			todos[indicemin]:=todos[indicemin]^.sig;
+		end;
+end;
+procedure imprimirnuevaL(L:lista);
+var
+	aux:lista;
+begin
+	aux:=L;
+	writeln;
+	writeln('La lista de todas las peliculas es: ');
+	while(aux<>nil)do begin
+		write('-',aux^.dato.codigo);
+		aux:=aux^.sig;
+		end;
+end;
+
+procedure merge(todos:vectorg;var NuevaL:lista);
+var
+	dmin:datos;
+	ult:lista;
+begin
+	minimo(todos,dmin);
+	while(dmin.codigo<>999)do begin
+		agregaratras(NuevaL,ult,dmin);
+		minimo(todos,dmin);
+		end;
+	imprimirnuevaL(NuevaL);
 end;
 
 procedure imprimirlistas(v:vectorg);
@@ -77,19 +136,22 @@ var
 	i:rango8;
 begin
 	for i:=1 to cantg do begin
+		writeln('lista ',i);
 		while(v[i]<>nil)do begin
-			writeln('-',v[i]^.dato.codigo);
+			write('-',v[i]^.dato.codigo);
 			v[i]:=v[i]^.sig;
 		end;
-		writeln('lista ',i,' finalizada');
+		writeln;
 	end;
 end;
 var
 	v:vectorg;
+	nl:lista;
 begin
+	nl:=nil;
 	inicializarlistas(v);
 	generarlistas(v);
 	imprimirlistas(v);
-end.		
-
+	merge(v,nl);
+end.
 		
